a8af45ca87ed1f36a92f12ed68fb01e89551d227
umayangana <umayanganaihalage@gmail.com>
2026-02-15 01:11:31 +0530

    Restructure auth folder and add animated login/register pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,38 @@
-import { Routes, Route } from "react-router-dom"
-import PublicLayout from "./layouts/PublicLayout"
-import Home from "./pages/public/Home"
-import About from "./pages/public/About"
-import Services from "./pages/public/Services"
-import OpeningScreen from "./pages/public/OpeningScreen"
+import { Routes, Route, useLocation } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
+
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import OpeningScreen from "./pages/public/OpeningScreen";
+import PublicLayout from "./layouts/PublicLayout";
+import Home from "./pages/public/Home";
+import About from "./pages/public/About";
+import Services from "./pages/public/Services";
 
 function App() {
+  const location = useLocation();
+
   return (
-    <Routes>
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
 
-      {/* Opening screen */}
-      <Route path="/" element={<OpeningScreen />} />
+        {/* Auth Pages */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
 
-      {/* Public Layout */}
-      <Route element={<PublicLayout />}>
+        {/* Opening Screen */}
+        <Route path="/" element={<OpeningScreen />} />
 
-        {/* Make Home also accessible at /home */}
-        <Route path="/home" element={<Home />} />
+        {/* Public Layout */}
+        <Route element={<PublicLayout />}>
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+        </Route>
 
-        <Route path="/about" element={<About />} />
-        <Route path="/services" element={<Services />} />
-
-      </Route>
-    </Routes>
-  )
+      </Routes>
+    </AnimatePresence>
+  );
 }
 
-export default App
+export default App;
