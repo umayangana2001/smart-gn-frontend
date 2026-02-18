7b4841a8a0f74cfbab67b6389c846e279a6ece97
aruni2001-dinushi <arunidinushi@gmail.com>
2026-02-18 22:13:37 +0530

    fix(services): update auth service with improved error handling

--- src/services/authService.js
+++ src/services/authService.js
@@ -12,3 +12,19 @@ export const registerUser = async (userData) => {
   const response = await API.post("/auth/user/register", userData);
   return response.data;
 };
+
+// 🔹 get single profile
+export const getUserProfile = async (userId) => {
+  const res = await api.get(`/user-profile/${userId}`);
+  return res.data;
+};
+
+export const getMyProfile = async (userId) => {
+  const res = await API.get(`/user-profile/${userId}`);
+  return res.data;
+};
+
+export const updateMyProfile = async (userId, data) => {
+  const res = await API.post(`/user-profile/${userId}`, data);
+  return res.data;
+};
