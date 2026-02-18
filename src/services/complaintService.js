906773c0de1799bccc742b9bae910113745c6644
rovidara <omindividara@gmail.com>
2026-02-18 23:54:02 +0530

    add createComplaint funtion

--- src/services/complaintService.js
+++ src/services/complaintService.js
@@ -1,3 +1,12 @@
 import api from "./api";
 
 
+export const createComplaint = async (formData) => {
+  const payload = {
+    title: formData.title,
+    description: formData.description,
+  };
+
+  const res = await api.post("/complaints", payload);
+  return res.data;
+};
